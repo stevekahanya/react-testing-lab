62f5e887c9ab68f629856bcfb330b21309fc8e24
stevekahanya <wakahusteve@gmail.com>
2026-02-24 08:53:54 +0300

    Format Transaction component for improved readability and consistency

--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 
-function Transaction({transaction}) {
+function Transaction({ transaction }) {
   return (
     <tr>
       <td>{transaction.date}</td>
       <td>{transaction.description}</td>
       <td>{transaction.category}</td>
       <td>{transaction.amount}</td>
+      <td>
+        {/* Empty cell or a delete button to match the header */}
+        <button className="ui mini red button">Delete</button>
+      </td>
     </tr>
   );
 }
